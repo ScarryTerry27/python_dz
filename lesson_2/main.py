f98161f0bff0d2c200722631f9452f6d281ec2ca
Ilyas Khasakhanov <clauster27@gmail.com>
2023-10-06 01:57:12 +0400

    z_1 z_2

---
+++ lesson_2/main.py
@@ -0,0 +1,20 @@
+# задание 1
+
+a, b, c = (int(input()) for _ in range(3))
+print(sum([a, b, c]), a * b * c, sep='\n')
+
+# задание 2
+a, b, c = (int(input()) for _ in range(3))
+print(a - b - c)
+
+# задание 3
+a, b = int(input()), int(input())
+print(a * b / 2)
+
+# задание 4
+
+print(f'To be\nor not\nto be')
+
+# задание 5
+
+print(f'''"Life is what happens\n\twhen\n\t\tyou're busy making other plans"\n\t\t\t\t\t\t\t\tJohn Lennon.''')
