42fddcdbc4f410b42819f83b8c14178a9431b841
Student <Student@sar64.local>
2023-12-20 20:27:26 +0400

    z20/12/2023

--- function_modul5_part1/main.py
+++ function_modul5_part1/main.py
@@ -1,53 +1,60 @@
-# модуль 5 функции Хасаханов
-from functools import reduce
+from faker import Faker
+from string import ascii_lowercase
+
+fake = Faker()
+
+with open('zadanie1_1', mode='w', encoding='utf-8') as f, open('zadanie1_2', mode='w', encoding='utf-8') as f1:
+    for _ in range(10):
+        print(fake.name(), file=f)
+        print(fake.name(), file=f1)
 
 
-def print_text():
-    # задание 1
-    print('''“Don't compare yourself with anyone in this world…
-    if you do so, you are insulting yourself.”
-                                            Bill Gates''')
+# 1 задание
+with open('zadanie1_1', mode='r', encoding='utf-8') as f, open('zadanie1_2', mode='r', encoding='utf-8') as f1:
+    file1 = [i.strip() for i in f.readlines()]
+    file2 = [i.strip() for i in f1.readlines()]
+    for i in range(len(file1)):
+        if file1[i] != file2[i]:
+            print(file1[i], file2[i], sep='\n')
 
 
-def check_even_in_interval(a, b):
-    # задание 2
-    print(*(i for i in range(a, b+1) if i % 2))
+# 2 задание
+with open('zadanie1_1', mode='r', encoding='utf-8') as f:
+    file1 = [i.strip() for i in f.readlines()]
+    count_symb = sum((len(i) for i in file1))
+    count_str = len(file1)
+    count_gl = sum((1 for i in file1 for l in i if l.lower() in 'eyuioa'))
+    count_sgl = sum((1 for i in file1 for l in i if l.lower() in set(ascii_lowercase) - set('eyuioa')))
+    count_dgt = sum((1 for i in file1 for l in i if l.lower() in '1234567890'))
+
+# 2 задание
+with open('new_file', mode='w', encoding='utf-8') as file:
+    print(count_symb, count_str, count_gl, count_sgl, count_dgt, file=file)
 
 
-def draw_square(length, symbol, fill=True):
-    # задание 3
-    if fill:
-        matrix = [[symbol for _ in range(length)] for _ in range(length)]
-    else:
-        matrix = [[
-            symbol if any((j in (0, length-1), i in (0, length-1))) else ' ' for j in range(length)] for i in range(length)
-        ]
-    for m in matrix:
-        print(*m)
+with open('zadanie1_1', mode='r', encoding='utf-8') as f:
+    file1 = [i.strip() for i in f.readlines()]
 
 
-def return_min(*args):
-    # задание 4
-    if len(args) > 5:
-        return 'Должно быть 5 элементов по условию заданий'
-    return min(args)
+# 3 задание
+with open('new_file1', mode='w', encoding='utf-8') as file:
+    print('\n'.join(file1[::-1]), file=file)
 
 
-def return_pr(a, b):
-    # задание 5
-    if a > b:
-        a, b = b, a
-    return reduce(lambda x, y: x * y, (range(a, b+1)))
+# 4 задание
+print(max(file1, key=lambda x: len(x)))
 
 
-# задание 6
-length_num = lambda x: len(str(x))
-
-# задание 7
-num_is_pal = lambda x: str(x) == str(x)[::-1]
-
-
+# 5 задание
+with open('zadanie1_1', mode='r', encoding='utf-8') as f:
+    file1 = f.read().lower()
+    word = input()
+    print(file1.count(word))
 
 
-
-
+# 6 задание
+    file1 = f.read().lower()
+    word1 = input()
+    word2 = input()
+    count = file1.count(word1)
+    print(file1.replace(word1, word2, count))
